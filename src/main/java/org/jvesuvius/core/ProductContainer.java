ec3f4b616179b20db6140b1d09c061ba6c323cdb
developer73 <developer73@>
2014-01-26 19:35:49 -0700

    Creating a view for an individual product.

---
+++ src/main/java/org/jvesuvius/core/ProductContainer.java
@@ -0,0 +1,63 @@
+package org.jvesuvius.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.jvesuvius.core.Database;
+import org.jvesuvius.core.Product;
+
+class ProductContainer {
+    public ArrayList<Product> getAll() {
+        ResultSet rs = getResultSetAll();
+        ArrayList<Product> products = new ArrayList<Product>();
+        try {
+            while(rs.next()) {
+                Product product = new Product(
+                    rs.getInt("product_id"),
+                    rs.getString("product_name"),
+                    rs.getDouble("product_price"),
+                    rs.getString("product_desc"));
+                products.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+    public Product getProductById(int id) {
+        ResultSet rs = getResultSetProductById(id);
+        Product product = null;
+        try {
+            rs.next();
+            product = new Product(
+                rs.getInt("product_id"),
+                rs.getString("product_name"),
+                rs.getDouble("product_price"),
+                rs.getString("product_desc"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return product;
+    }
+    private ResultSet getResultSetAll() {
+        Database db = new Database();
+        String template = "select %s from %s";
+        String query = String.format(template,
+            "product_id, product_name, product_price, product_desc",
+            "product");
+        ResultSet rs = db.getResultSet(query);
+        return rs;
+    }
+    private ResultSet getResultSetProductById(int id) {
+        Database db = new Database();
+        String template = "select %s from %s where %s%s";
+        String query = String.format(template,
+            "product_id, product_name, product_price, product_desc",
+            "product",
+            "product_id = ",
+            id);
+        ResultSet rs = db.getResultSet(query);
+        return rs;
+    }
+}
--- src/main/java/org/jvesuvius/core/ProductView.java
+++ src/main/java/org/jvesuvius/core/ProductView.java
@@ -1,22 +1,21 @@
 package org.jvesuvius.core;
 
-import java.util.ArrayList;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-import org.jvesuvius.core.Product;
 import org.jvesuvius.core.ProductContainer;
 import org.jvesuvius.core.ToJsonConverter;
 
-@Path("list")
-public class ProductListView {
+@Path("{id}")
+public class ProductView {
     @GET
     @Produces("application/json")
-    public String getMessage() {
+    public String getMessage(@PathParam("id") int id) {
         ProductContainer pc = new ProductContainer();
-        ArrayList<Product> products = pc.getAll();
+        Product product = pc.getProductById(id);
         ToJsonConverter conv = new ToJsonConverter();
-        return conv.convert(products);
+        return conv.convert(product);
     }
 }
